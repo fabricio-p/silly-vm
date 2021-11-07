2a133c3dc698baa41485e814a0cfca3784158b02
Fabricio Pashaj <fabriciopashaj.dev@gmail.com>
2021-11-07 15:03:02 +0100

    feat(macros): Added logging macros and instruction and type name shortcuts.

--- include/macros.h
+++ include/macros.h
@@ -3,13 +3,26 @@
 
 #define __inline__ static inline __attribute__((always_inline))
 
+#ifdef ENV_DEV
 #define LOG_DEBUG(...) {              \
   printf("[DEBUG(%d)]: ", __LINE__);  \
   printf(__VA_ARGS__);                \
   printf("\n");                       \
 }
 #define LOG_EVAL(expr, fmt) {         \
-  LOG_DEBUG(#expr " = %" fmt, expr);\
+  LOG_DEBUG(#expr " = %" fmt, expr);  \
 }
+#else
+#define LOG_DEBUG(...)
+#define LOG_EVAL(expr, fmt)
+#endif /* ENV_DEV */
+
+#define LOG_ERROR(...) {              \
+  fprintf(stderr, "[ERROR]: ");       \
+  fprintf(stderr, __VA_ARGS__);       \
+}
+
+#define INSTR(instr) SILLY_INSTR_##instr
+#define TYPE(type) SILLY_TYPE_##type
 
 #endif /* MACROS_H */
