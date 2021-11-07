00815e05ed9b948b816f5077f8cb00f386878afe
Fabricio Pashaj <fabriciopashaj.dev@gmail.com>
2021-11-07 20:43:21 +0100

    feat: Added header for platform/architecture specific configurations.

---
+++ include/config.h
@@ -0,0 +1,30 @@
+#ifndef __SILLY_CONFIG_H__
+#define __SILLY_CONFIG_H__
+
+#if !defined(BUILD_TARGET_ARM) && !defined(BUILD_TARGET_AMD64)        \
+  && !defined(BUILD_TARGET_AARCH64) && !defined(BUILD_TARGET_X86_32)  \
+  && !defined(BUILD_TARGET_X86_64)
+#if defined(__arm__) || defined(__arm)
+#define BUILD_TARGET_ARM
+#elif defined(__amd64__) || defined(__amd64)
+#define BUILD_TARGET_AMD64
+#elif defined(__aarch64__) || defined(__aarch64)
+#define BUILD_TARGET_AARCH64
+#elif defined(__i386__) || defined(__i386) || defined(i386)
+#define BUILD_TARGET_X86_32
+#elif defined(_x86_64__) | defined(__x86_64)
+#define BUILD_TARGET_X86_64
+#else
+#error "Build target is not set"
+#endif
+#endif
+
+
+#if defined(BUILD_TARGET_AARCH64) || defined(BUILD_TARGET_X86_32)  \
+  || defined(BUILD_TARGET_X86_64)
+#define SILLY_CPU_UNALIGNED_POINTERS 1
+#else
+#define SILLY_CPU_UNALIGNED_POINTERS 0
+#endif
+
+#endif /* __SILLY_CONFIG_H__ */
