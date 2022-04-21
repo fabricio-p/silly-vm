a7b492562e02aeb7229acd30e72b839305ed01e7
Fabricio Pashaj <fabriciopashaj.dev@gmail.com>
2022-04-21 16:57:56 +0200

    feat: Changed layout of VM internals
    BREAKING CHANGE

--- include/silly.h
+++ include/silly.h
@@ -2,13 +2,13 @@
 #define SILLY_H
 #include <stdlib.h>
 #include <stdint.h>
-// #include <c-bump-alloc/lib.h>
+#include <c-arena-alloc/lib.h>
 
 // Typedefs of basic primitive types and structs
-typedef unsigned char      U8;
-typedef signed   char      S8;
-typedef unsigned short     U16;
-typedef signed   short     S16;
+typedef uint8_t            U8;
+typedef int8_t             S8;
+typedef uint16_t           U16;
+typedef int16_t            S16;
 typedef unsigned long      U32;
 typedef signed   long      S32;
 typedef unsigned long long U64;
@@ -24,8 +24,13 @@ typedef S32        Ssize;
 typedef char       *Str;
 typedef char const *CStr;
 typedef void       *voidptr;
-typedef U8         SValueKind;
+typedef U8         STaggedValueKind;
 typedef U8         Bool;
+
+typedef struct sized_str_t {
+  char const *str;
+  Uint       len;
+} SizedStr;
 
 #ifdef DEBUG // so gdb shows us this instead of just integers during debug
 #define E(e) SILLY_E_##e
@@ -36,7 +41,14 @@ typedef enum {
   E(SO),
   E(CDE),
   E(CRASH),
-  E(PARSE_INVALID_TYPE_SIZE)
+  E(INCORRECT_SECTION_SIZE),
+  E(INVALID_TYPE_SIZE),
+  E(INCORRECT_TYPE_COUNT),
+  E(INCORRECT_FUNCTION_COUNT),
+  E(INVALID_TYPE_INDEX),
+  E(EXHAUSTED_STACK),
+  E(INVALID_OPERAND_KIND),
+  E(OOB_LOCAL_ACCESS)
 } SStatus;
 #undef E
 #else
@@ -47,126 +59,142 @@ typedef enum {
 #define SILLY_E_CDE   4 // (C)all (D)epth (E)xceeded
 #define SILLY_E_CRASH 5
 // Errors that occur only during bytecode parsing
-#define SILLY_E_PARSE_INVALID_TYPE_SIZE 6
+#define SILLY_E_INCORRECT_SECTION_SIZE   6
+#define SILLY_E_INVALID_TYPE_SIZE        7
+#define SILLY_E_INCORRECT_TYPE_COUNT     8
+#define SILLY_E_INCORRECT_FUNCTION_COUNT 9
+#define SILLY_E_INVALID_TYPE_INDEX       10
+#define SILLY_E_EXHAUSTED_STACK          11
+#define SILLY_E_INVALID_OPERAND_KIND     12
+#define SILLY_E_OOB_LOCAL_ACCESS         13
 
 typedef Ssize      SStatus;
 #endif
 
-/* typedef struct PStr {
-  U32 len;
-  U8  str[1];
-} PStr; */
-
 // Internal VM structs
+
+typedef struct SStackFrame SStackFrame;
+typedef struct SCallFrame  SCallFrame;
+typedef struct SEnv        SEnv;
+typedef struct SStack      SStack;
+
 typedef struct SType {
-  U16 param_count;
-  U16 result_count;
-  U8  types[1];
+  U16       param_count;
+  U16       result_count;
+  U8  const *types;
 } SType;
 
 typedef struct SFunc {
   SType const *type;
   U8    const *code;
   U8    const *code_end;
-  // PStr  const *name;
+  SizedStr    name;
+  void        (*native)(SCallFrame *);
   struct {
-    Usize len;
-    CStr  str;
-  }           name;
-  /* struct {
-    Usize max : 15;
-    Usize is_reported : 1;
-  } stack_usage; */
-  // void  (*native)(SCallFrame *);
+    Uint code_offset; // from the start of the code section
+    Uint code_end;
+    Uint type_idx;
+    Uint offset; // from the start of the function section
+  }           raw;
+  struct {
+    Uint stack_use;
+    Bool tail_opt;
+    Bool recursive;
+  }           meta;
+  struct {
+    U8  const *types;
+    U16       count;
+  }           locals;
 } SFunc;
-typedef SFunc **SFuncTable;
+typedef ARENA_PTR_T(silly_func_ptr_t, SFunc) SFuncPtr;
 
-// A stack value
-typedef struct SValue {
-  union {
-    U32 u32;
-    S32 s32;
-    F32 f32;
-    U64 u64;
-    S64 s64;
-    F64 f64;
-  };
-  SValueKind kind;
-  U8 __padding[16 - sizeof(U64) - sizeof(SValueKind)];
+// #include "functable.h"
+
+typedef union SValue {
+  U32 u32;
+  S32 s32;
+  F32 f32;
+  U64 u64;
+  S64 s64;
+  F64 f64;
 } SValue;
 
-typedef struct SStackFrame SStackFrame;
-typedef struct SCallFrame  SCallFrame;
-typedef struct SEnv        SEnv;
-
-typedef struct SStack {
-  SValue     *data;
-  U32        size;
-  U32        max_size;
-  SCallFrame *frames;
-  U32        call_depth;
-} SStack;
+typedef struct STaggedValue {
+  SValue           v;
+  STaggedValueKind kind;
+  U8               __padding[sizeof(U64) - sizeof(STaggedValueKind)];
+} STaggedValue;
 
 typedef struct SMemory {
   U32 page_count;
   U32 max_pages;
-  U8  data;
+  U8  *data;
 } SMemory;
 
 struct SCallFrame {
   // the value at the stack top
-  SValue      cache;
+  STaggedValue cache;
   // (s)tack (l)ocals, the pointer to where the locals start
-  SValue      *sl;
+  STaggedValue *sl;
   // (s)tack (b)ottom, the pointer to where the locals end and operational
   // stack where you push and pop starts
-  SValue      *sb;
+  STaggedValue *sb;
   // (s)tack (t)op, the pointer at the slot right after the last used one
-  SValue      *st;
+  STaggedValue *st;
   // the pointer to the function that called
-  SFunc       *function;
-  SStack      *stack;
-  U32         frame_index;
+  SFunc        *function;
+  SStack       *stack;
+  U32          frame_index;
   // will tell at which opcode failure occurred if it occurs
-  U8 const    *ip;
-  // the size of the stack in SValues it can fit
-  U32         size;
+  U8 const     *ip;
+  // the size of the stack in STaggedValues it can fit
+  U32          size;
 };
 
-typedef struct SModuleInfo SModuleInfo;
+#define SILLY_STACK_SIZE 0x800
+#define SILLY_CALL_DEPTH 0x100
+struct SStack {
+  STaggedValue data  [SILLY_STACK_SIZE];
+  SCallFrame   frames[SILLY_CALL_DEPTH];
+  SCallFrame   *frame;
+};
+
+typedef struct SSecInfo {
+  Uint size;
+  Uint item_count;
+} SSecInfo;
 
 typedef struct SModule {
-  CStr       path;
+  CStr  path;
   struct {
     U8  *buffer;
-    U32 type_sec_size;
-    U32 func_sec_size;
-    U32 data_sec_size;
-    U32 code_sec_size;
+    union {
+      struct {
+        SSecInfo type;
+        SSecInfo func;
+        SSecInfo data;
+        SSecInfo code;
+      }        seci;
+      SSecInfo seci_arr[4];
+    };
     struct {
       U16 min;
       U16 max;
     }   mem_cfg;
-  }          raw;
-  struct {
-    voidptr types_buffer;
-    voidptr bytecode_buffer;
-    U8      *data_buffer;
-  }          loaded;
-  SType      **types;
-  U8         **data; // like, from the data section. idk why I need this
-  SFuncTable func_table;
-  SFuncTable exports;
+  }     raw;
+  SType *types;
+  SFunc *functions;
+  Bool  initted;
 } SModule;
+typedef ARENA_PTR_T(silly_module_ptr_t, SModule) SModulePtr;
 
 typedef struct AllocNode AllocNode;
 
 struct SEnv {
-  SStack    stack;
-  SMemory   memory;
-  SModule   *module_pool;
-  SFunc     *func_pool;
-  AllocNode *type_pool_chain;
+  SStack     stack;
+  SMemory    memory;
+  ArenaAlloc mod_pool;
+  SModule    *main_mod;
 };
 
 #endif /* SILLY_H */
