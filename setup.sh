352c9a1606ec3f787cbe8ac0abcf0891a9c5992a
Fabricio Pashaj <fabriciopashaj.dev@gmail.com>
2022-04-21 17:06:07 +0200

    feat(setup.sh): Added commands for downloading new libraries

--- setup.sh
+++ setup.sh
@@ -6,6 +6,8 @@ mkdir -p cake_libs/
 current_dir=`pwd`
 cd cake_libs
 git clone https://github.com/fabriciopashaj/c-vector
+git clone https://github.com/fabriciopashaj/c-ansi-sequences
+git clone https://github.com/fabriciopashaj/c-arena-alloc
 # git clone https://github.com/fabriciopashaj/c-string
 
 cd $current_dir
